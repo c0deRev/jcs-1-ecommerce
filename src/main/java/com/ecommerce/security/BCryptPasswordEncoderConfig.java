b484effa8276f1eab2ded160a11a0fe785a62908
coder <79178809+c0deblack@users.noreply.github.com>
2023-05-05 16:54:34 -0400

    Added registration using UserDetailService and InMemoryUserDetailsManager

---
+++ src/main/java/com/ecommerce/security/BCryptPasswordEncoderConfig.java
@@ -0,0 +1,24 @@
+package com.ecommerce.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * Configuration class that creates a {@link BCryptPasswordEncoder} instance as a {@link PasswordEncoder} bean.
+ */
+@Configuration
+public class BCryptPasswordEncoderConfig {
+
+  /**
+   * Creates a {@link BCryptPasswordEncoder} instance as a {@link PasswordEncoder} bean.
+   * @return A {@link PasswordEncoder} instance that uses the BCrypt algorithm to encode passwords.
+  */
+  @Bean
+  @Primary
+  public static PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}
---
+++ src/main/java/com/ecommerce/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.ecommerce.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth ->
+                    auth
+                        .requestMatchers("/register")
+                        .permitAll()
+                )
+                .csrf((csrf) -> csrf
+                        .ignoringRequestMatchers("/**")
+                ).build();
+    }
+
+}
